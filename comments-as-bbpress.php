7e77cd3808726370f5441e0b2fdba93960869beb
Miguel Lezama <miguel@automattic.com>
2023-01-17 10:00:42 -0300

    Make bbpress believe we are dealing with a single specific subforum

--- comments-as-bbpress.php
+++ comments-as-bbpress.php
@@ -10,14 +10,20 @@ function bbp_subforum_replacer_custom_comments_template() {
 	$subforum_id = get_post_meta( $post->ID, 'bbp_subforum_id', true );
 	if ( empty( $subforum_id ) ) {
 		$subforum_id = bbp_insert_forum( array(
-			'post_parent' => 0,
 			'post_title' => $post->post_title,
 			'post_content' => $post->post_excerpt,
 			'post_status' => 'publish',
 		) );
 		update_post_meta( $post->ID, 'bbp_subforum_id', $subforum_id );
 	}
+	add_filter( 'bbp_get_forum_id', function() use ( $subforum_id ) {
+		return $subforum_id;
+	} );
+	$post = get_post( $subforum_id, OBJECT );
+	setup_postdata( $post );
+	bbp_set_query_name( 'bbp_single_forum' );
 	return dirname(__FILE__) . '/templates/custom-comments.php';
 }
+
 
 add_filter( 'comments_template', 'bbp_subforum_replacer_custom_comments_template' );

525c10a027031fffa0207711c51d1ca4171bfe6e
Miguel Lezama <miguel@automattic.com>
2023-01-17 10:00:56 -0300

    simplify the template for now

--- templates/custom-comments.php
+++ templates/custom-comments.php
@@ -1,9 +1,7 @@
 <?php
 
-/** this is based on bbPress single-forum.php template */
-
 /**
- * Single Forum Content Part
+ * Forums Loop - Single Forum
  *
  * @package bbPress
  * @subpackage Theme
@@ -11,39 +9,6 @@
 
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
-
+bbp_get_template_part( 'content', 'single-forum' );
 ?>
 
-<div id="bbpress-forums" class="bbpress-wrapper">
-
-	<?php do_action( 'bbp_template_before_single_forum' ); ?>
-
-	<?php if ( post_password_required() ) : ?>
-
-		<?php bbp_get_template_part( 'form', 'protected' ); ?>
-
-	<?php else : ?>
-
-		<?php if ( ! bbp_is_forum_category() && bbp_has_topics() ) : ?>
-
-			<?php bbp_get_template_part( 'pagination', 'topics'    ); ?>
-
-			<?php bbp_get_template_part( 'loop',       'topics'    ); ?>
-
-			<?php bbp_get_template_part( 'pagination', 'topics'    ); ?>
-
-			<?php bbp_get_template_part( 'form',       'topic'     ); ?>
-
-		<?php elseif ( ! bbp_is_forum_category() ) : ?>
-
-			<?php bbp_get_template_part( 'feedback',   'no-topics' ); ?>
-
-			<?php bbp_get_template_part( 'form',       'topic'     ); ?>
-
-		<?php endif; ?>
-
-	<?php endif; ?>
-
-	<?php do_action( 'bbp_template_after_single_forum' ); ?>
-
-</div>

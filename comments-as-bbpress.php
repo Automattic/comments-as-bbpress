366691682ac39243c85e0028af6a3f02167b1cab
Miguel Lezama <miguel@automattic.com>
2023-01-18 07:45:08 -0300

    Make it easier to navigate

--- comments-as-bbpress.php
+++ comments-as-bbpress.php
@@ -10,8 +10,8 @@ function bbp_subforum_replacer_custom_comments_template() {
 	$subforum_id = get_post_meta( $post->ID, 'bbp_subforum_id', true );
 	if ( empty( $subforum_id ) ) {
 		$subforum_id = bbp_insert_forum( array(
-			'post_title' => $post->post_title,
-			'post_content' => $post->post_excerpt,
+			'post_title' => sprintf( '<a href="%s">%s</a>', esc_url( get_permalink( $post->ID ) ), $post->post_title ),
+			'post_content' => get_post_embed_html( 600, 400 ),
 			'post_status' => 'publish',
 		) );
 		update_post_meta( $post->ID, 'bbp_subforum_id', $subforum_id );

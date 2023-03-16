0c2d574b4080c34ce6a79eac4160aaa16d3acf7e
Miguel Lezama <miguel@automattic.com>
2023-03-16 11:26:59 -0300

    use the excerpt instead

--- templates/comments-as-bbpress-loop-single-topic.php
+++ templates/comments-as-bbpress-loop-single-topic.php
@@ -13,11 +13,11 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <ul id="bbp-topic-<?php bbp_topic_id(); ?>" <?php bbp_topic_class(); ?>>
-	<li class="bbp-topic-title">
+    <li class="bbp-topic-title">
 
 		<?php do_action( 'bbp_theme_before_topic_title' ); ?>
 
-		<a class="bbp-topic-permalink" href="<?php bbp_topic_permalink(); ?>"><?php bbp_topic_title(); ?></a>
+        <a class="bbp-topic-permalink" href="<?php bbp_topic_permalink(); ?>"><?php bbp_topic_title(); ?></a>
 
 		<?php do_action( 'bbp_theme_after_topic_title' ); ?>
 
@@ -25,13 +25,11 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php do_action( 'bbp_theme_before_topic_meta' ); ?>
 
-		<?php bbp_get_template_part( 'content', 'single-topic-lead' ); ?>
-
-		<p class="bbp-topic-meta">
+        <p class="bbp-topic-meta">
 
 			<?php do_action( 'bbp_theme_before_topic_started_by' ); ?>
 
-			<span class="bbp-topic-started-by"><?php printf( esc_html__( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?></span>
+            <span class="bbp-topic-started-by"><?php printf( esc_html__( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?></span>
 
 			<?php do_action( 'bbp_theme_after_topic_started_by' ); ?>
 
@@ -39,24 +37,24 @@ defined( 'ABSPATH' ) || exit;
 
 				<?php do_action( 'bbp_theme_before_topic_started_in' ); ?>
 
-				<span class="bbp-topic-started-in"><?php printf( esc_html__( 'in: %1$s', 'bbpress' ), '<a href="' . bbp_get_forum_permalink( bbp_get_topic_forum_id() ) . '">' . bbp_get_forum_title( bbp_get_topic_forum_id() ) . '</a>' ); ?></span>
+                <span class="bbp-topic-started-in"><?php printf( esc_html__( 'in: %1$s', 'bbpress' ), '<a href="' . bbp_get_forum_permalink( bbp_get_topic_forum_id() ) . '">' . bbp_get_forum_title( bbp_get_topic_forum_id() ) . '</a>' ); ?></span>
 				<?php do_action( 'bbp_theme_after_topic_started_in' ); ?>
 
 			<?php endif; ?>
 
-		</p>
+        </p>
 
 		<?php do_action( 'bbp_theme_after_topic_meta' ); ?>
 
 		<?php bbp_topic_row_actions(); ?>
 
-	</li>
+    </li>
 
-	<li class="bbp-topic-voice-count"><?php bbp_topic_voice_count(); ?></li>
+    <li class="bbp-topic-voice-count"><?php bbp_topic_voice_count(); ?></li>
 
-	<li class="bbp-topic-reply-count"><?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></li>
+    <li class="bbp-topic-reply-count"><?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></li>
 
-	<li class="bbp-topic-freshness">
+    <li class="bbp-topic-freshness">
 
 		<?php do_action( 'bbp_theme_before_topic_freshness_link' ); ?>
 
@@ -64,14 +62,22 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php do_action( 'bbp_theme_after_topic_freshness_link' ); ?>
 
-		<p class="bbp-topic-meta">
+        <p class="bbp-topic-meta">
 
 			<?php do_action( 'bbp_theme_before_topic_freshness_author' ); ?>
 
-			<span class="bbp-topic-freshness-author"><?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'size' => 14 ) ); ?></span>
+            <span class="bbp-topic-freshness-author"><?php bbp_author_link( array(
+					'post_id' => bbp_get_topic_last_active_id(),
+					'size'    => 14
+				) ); ?></span>
 
 			<?php do_action( 'bbp_theme_after_topic_freshness_author' ); ?>
 
-		</p>
-	</li>
+        </p>
+
+        <div class="bbp-topic-excerpt">
+			<?php bbp_reply_excerpt( bbp_get_topic_last_active_id(), bbp_get_topic_id() ); ?>
+        </div><!-- .bbp-topic-excerpt -->
+
+    </li>
 </ul><!-- #bbp-topic-<?php bbp_topic_id(); ?> -->
